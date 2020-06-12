60d3d48cf9b7fda4c001b2228e44037d27b4ae86
João Pedro Purcinelli <53058735+jowpurcinelli@users.noreply.github.com>
2020-06-12 02:26:59 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+//That is just an code anotation file, I would like to remember during the development of this project.
+
+
+
+
+
+
+$('.sequenced.images .image')
+  .transition({
+    debug     : true,
+    animation : 'jiggle',
+    duration  : 500,
+    interval  : 200
+  })
+;
+
+
+
